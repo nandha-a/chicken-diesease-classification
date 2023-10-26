ce8ae46162cb9d4b79d5194df9c433ae02a144da
Nandha_A <nandhakumara56@gmail.com>
2023-10-26 14:03:08 +0530

    prepare callbacks

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -19,3 +19,8 @@ class PrepareBaseModelConfig:
     params_weight: str
     params_classes: int
     
+@dataclass
+class PrepareCallbacksConfig:
+    root_dir: Path
+    tensorboard_root_log_dir: Path
+    checkpoint_model_filepath: Path
