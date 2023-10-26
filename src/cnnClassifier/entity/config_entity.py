e83c3ed851663bf65bf8685fa90d6776fa20b59a
Nandha_A <nandhakumara56@gmail.com>
2023-10-26 21:16:09 +0530

    Model training pipeline

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -19,8 +19,19 @@ class PrepareBaseModelConfig:
     params_weight: str
     params_classes: int
     
-@dataclass
+@dataclass(frozen=True)
 class PrepareCallbacksConfig:
     root_dir: Path
     tensorboard_root_log_dir: Path
     checkpoint_model_filepath: Path
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epochs: int
+    params_batch_size: int
+    params_is_augumentation: bool
+    params_image_size: list
